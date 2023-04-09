e4d20c91953b8ee9631329de348c289e65c10347
Torezhan Botakanov <bottorezhan@gmail.com>
2023-04-09 11:05:35 +0600

    bully final updates

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <vector>
 #include <iostream>
 #include <list>
+#include <tuple>
+#include <cassert>
 
 using namespace std;
 
@@ -23,6 +25,8 @@ struct Puck
 
 	// Puck color
 	int color;
+
+	int index;
 };
 
 /** Simple representation for a bumper. */
@@ -33,6 +37,10 @@ struct Bumper
 
 	// Bumper velocity
 	Vector2D vel;
+
+	int last_target;
+
+	int index = -1;
 };
 
 /** Simple representation for a sled. */
@@ -47,6 +55,96 @@ struct Sled
 	double dir;
 };
 
+struct GameState
+{
+
+public:
+	vector<Puck> my_pucks, en_pucks, nu_pucks, all_pucks;
+	vector<Bumper> my_blist, en_blist;
+	Sled my_sled, en_sled;
+
+	void read()
+	{
+		int n;
+
+		// Read all the puck locations.
+		cin >> n;
+		my_pucks.resize(0);
+		en_pucks.resize(0);
+		nu_pucks.resize(0);
+		all_pucks.resize(n);
+
+		for (int i = 0; i < n; i++)
+		{
+			Puck puck;
+			cin >> puck.pos.x >> puck.pos.y >> puck.vel.x >> puck.vel.y >> puck.color;
+			puck.index = i;
+
+			all_pucks[i] = puck;
+
+			if (puck.color == 0)
+			{
+				my_pucks.push_back(puck);
+			}
+			else if (puck.color == 1)
+			{
+				en_pucks.push_back(puck);
+			}
+			else
+			{
+				nu_pucks.push_back(puck);
+			}
+		}
+
+		// Read all the bumper locations.
+		cin >> n;
+		assert(n == 4);
+		my_blist.resize(2);
+		en_blist.resize(2);
+
+		for (int i = 0; i < n; i++)
+		{
+			Bumper &bumper = (i < 2 ? my_blist[i] : en_blist[i - 2]);
+			cin >> bumper.pos.x >> bumper.pos.y >> bumper.vel.x >> bumper.vel.y;
+
+			bumper.index = i;
+		}
+
+		// Read all the sled locations.
+		cin >> n;
+		assert(n == 2);
+
+		for (int i = 0; i < n; i++)
+		{
+			Sled &sled = (i == 0 ? my_sled : en_sled);
+			cin >> sled.pos.x >> sled.pos.y >> sled.dir;
+
+			// Just ignore the path history for this sled.
+			int m;
+			cin >> m;
+
+			sled.path.resize(m);
+			for (int j = 0; j < m; j++)
+			{
+				cin >> sled.path[j].x;
+				cin >> sled.path[j].y;
+			}
+		}
+	}
+};
+
+pair<Vector2D, double> circumscribed_circle(Vector2D p1, Vector2D p2, Vector2D p3)
+{
+	double a = sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	double b = sqrt((p3.x - p1.x) * (p3.x - p1.x) + (p3.y - p1.y) * (p3.y - p1.y));
+	double c = sqrt((p3.x - p2.x) * (p3.x - p2.x) + (p3.y - p2.y) * (p3.y - p2.y));
+	double radius = (a * b * c) / (sqrt((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c)));
+	double d = 2 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+	double xp = ((p1.x * p1.x + p1.y * p1.y) * (p2.y - p3.y) + (p2.x * p2.x + p2.y * p2.y) * (p3.y - p1.y) + (p3.x * p3.x + p3.y * p3.y) * (p1.y - p2.y)) / d;
+	double yp = ((p1.x * p1.x + p1.y * p1.y) * (p3.x - p2.x) + (p2.x * p2.x + p2.y * p2.y) * (p1.x - p3.x) + (p3.x * p3.x + p3.y * p3.y) * (p2.x - p1.x)) / d;
+
+	return {{xp, yp}, radius};
+}
 /** Return a copy of x that's constrained to be between low and high. */
 double clamp(double x, double low, double high)
 {
@@ -65,10 +163,10 @@ double clamp(double x, double low, double high)
 	looks like the next move will take us through the target
 	location.  */
 bool run_to(Vector2D const &pos,
-		   Vector2D const &vel,
-		   Vector2D const &target,
-		   Vector2D &force,
-		   double epsilon = 0.1)
+			Vector2D const &vel,
+			Vector2D const &target,
+			Vector2D &force,
+			double epsilon = 0.1)
 {
 	// Compute a vector that will move self toward the target point.
 	Vector2D direction = target - pos;
@@ -98,111 +196,89 @@ bool run_to(Vector2D const &pos,
 	return false;
 }
 
-/** Return true if a puck at position pos is the responsibility of the
-	bumper with index bdex. */
-bool my_side(int bdex, Vector2D pos)
-{
-	if (bdex == 0 && pos.y < 330)
-		return true;
-	if (bdex == 1 && pos.y > 470)
-		return true;
-	return false;
-}
-
-pair<Vector2D, double> circumscribed_circle(Vector2D p1, Vector2D p2, Vector2D p3)
-{
-	double a = sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-	double b = sqrt((p3.x - p1.x) * (p3.x - p1.x) + (p3.y - p1.y) * (p3.y - p1.y));
-	double c = sqrt((p3.x - p2.x) * (p3.x - p2.x) + (p3.y - p2.y) * (p3.y - p2.y));
-	double radius = (a * b * c) / (sqrt((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c)));
-	double d = 2 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
-	double xp = ((p1.x * p1.x + p1.y * p1.y) * (p2.y - p3.y) + (p2.x * p2.x + p2.y * p2.y) * (p3.y - p1.y) + (p3.x * p3.x + p3.y * p3.y) * (p1.y - p2.y)) / d;
-	double yp = ((p1.x * p1.x + p1.y * p1.y) * (p3.x - p2.x) + (p2.x * p2.x + p2.y * p2.y) * (p1.x - p3.x) + (p3.x * p3.x + p3.y * p3.y) * (p2.x - p1.x)) / d;
-
-	return {{xp, yp}, radius};
-}
-
 pair<Vector2D, double> find_center(vector<Vector2D> path)
 {
-	
-	if (path.size() < 3) {
+
+	if (path.size() < 3)
+	{
 		return {(path.front() - path.back()) * 0.5, 0};
 	}
 
 	return circumscribed_circle(path.front(), path[path.size() / double(2)], path.back());
 }
 
-struct GameState
+pair<double, Puck> get_closest_puck(const vector<Puck> &plist, Vector2D pos)
 {
-	vector<Puck> plist;
-	vector<Bumper> blist;
-	vector<Sled> slist;
 
-	void read() {
-		int n;
+	double distance = -1;
+	Puck cp;
 
-		// Read all the puck locations.
-		cin >> n;
-		plist.resize(n);
-		for (int i = 0; i < n; i++)
+	for (const auto &p : plist)
+	{
+		double dist = (p.pos - pos).mag();
+
+		if (distance == -1 || dist < distance)
 		{
-			Puck &puck = plist[i];
-			cin >> puck.pos.x >> puck.pos.y >> puck.vel.x >> puck.vel.y >> puck.color;
-		}
-
-		// Read all the bumper locations.
-		cin >> n;
-		blist.resize(n);
-		for (int i = 0; i < n; i++)
-		{
-			Bumper &bumper = blist[i];
-			cin >> bumper.pos.x >> bumper.pos.y >> bumper.vel.x >> bumper.vel.y;
-		}
-
-		// Read all the sled locations.
-		cin >> n;
-		slist.resize(n);
-		for (int i = 0; i < n; i++)
-		{
-			Sled &sled = slist[i];
-			cin >> sled.pos.x >> sled.pos.y >> sled.dir;
-
-			// Just ignore the path history for this sled.
-			int m;
-			cin >> m;
-
-			sled.path.resize(m);
-			for (int j = 0; j < m; j++)
-			{
-				cin >> sled.path[j].x;
-				cin >> sled.path[j].y;
-			}
+			distance = dist;
+			cp = p;
 		}
 	}
-};
 
-Vector2D bully_tactic(const Bumper& bumper, const GameState& state) {
+	return {distance, cp};
+}
+
+Vector2D bully_tactic(Bumper &bumper, const GameState &state)
+{
 	Vector2D force(0, 0);
 
-	int ENEMY_SLED = 1;
-	Vector2D target(400, 400);
-	
-	if (state.slist[ENEMY_SLED].path.empty()) {
-		target = state.slist[ENEMY_SLED].pos;
+	const Sled &enemy_sled = state.en_sled;
+	Vector2D target = enemy_sled.pos;
+
+	// if (!enemy_sled.path.empty()) {
+	// 	target = enemy_sled.pos;
+	// 	Vector2D center(0, 0);
+	// 	double radius;
+	// 	tie(center, radius) = find_center(enemy_sled.path);
+
+	// 	if (radius > 100) {
+	// 		center = enemy_sled.pos;
+	// 	}
+
+	// 	target = center;
+	// 	cerr << "Circle radius: " << radius << " coords: " << center.x << ' ' << center.y << '\n';
+	// }
+
+
+	cerr << bumper.last_target << '\n';
+
+	bool target_existed = false;
+	Puck closest_puck;
+	double distance_to_closest_puck;
+
+	if (bumper.last_target != -1 && (state.all_pucks[bumper.last_target].pos - enemy_sled.pos).mag() < 200) {
+		closest_puck = state.all_pucks[bumper.last_target];
+		distance_to_closest_puck = (state.all_pucks[bumper.last_target].pos - enemy_sled.pos).mag();
+		target_existed = true;
 	} else {
-		auto center = find_center(state.slist[ENEMY_SLED].path);
-
-		if (center.second > 100) {
-			center.first = state.slist[ENEMY_SLED].pos;
-		}
-
-		target = center.first;
+		tie(distance_to_closest_puck, closest_puck) = get_closest_puck(state.en_pucks, enemy_sled.pos);
 	}
 
-	run_to(bumper.pos, bumper.vel, target, force);
+	if ((!target_existed and distance_to_closest_puck < 100) or (target_existed and distance_to_closest_puck < 200))
+	{
+		target = closest_puck.pos;
+		bool res = run_to(bumper.pos, bumper.vel, target, force, enemy_sled.dir);
+		cerr << res << '\n';
+		// run_to(bumper.pos, bumper.vel, target, force, enemy_sled.dir);
 
-	// force = (target - bumper.pos).limit(BUMPER_FORCE_LIMIT);
+		// force = (target - bumper.pos).limit(BUMPER_FORCE_LIMIT);
+		bumper.last_target = closest_puck.index;
+		
+		// return force;
+	} else {
+		bumper.last_target = -1;
+	}
 
+	force = (target - bumper.pos).limit(BUMPER_FORCE_LIMIT);
 	return force;
 }
 
@@ -223,18 +299,26 @@ int main()
 		// Choose a move for each sled.
 		for (int i = 0; i < 2; i++)
 		{
-			Bumper &bumper = state.blist[i];
+			Bumper &bumper = state.my_blist[i];
 			Vector2D force(0, 0);
 			bool is_bully = (i == 1);
 
-			if (is_bully) {
+			if (is_bully)
+			{
 				force = bully_tactic(bumper, state);
-			} else {
+			}
+			else
+			{
 				// force = bully_tactic(bumper, state);
 			}
 
+			// cerr << force.x << " " << force.y << "\n";
 			cout << force.x << " " << force.y << " ";
 		}
+
+		const auto& sled = state.my_sled;
+
+		// cancel 
 
 		// Just keep making circles and march across the playing field.
 		double sledDir = 0;
